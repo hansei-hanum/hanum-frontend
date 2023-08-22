fcfee9006705110de480aaea03e72e3ae882bac4
최근원 <kidjustinchoi@gmail.com>
2023-08-22 14:24:54 +0900

    feat(auth): add code input layer in verify code screnn

--- apps/auth/src/screens/verifyCode/index.tsx
+++ apps/auth/src/screens/verifyCode/index.tsx
@@ -3,26 +3,68 @@ import { colors } from "@hanum/styles";
 import React, { useState } from "react";
 import { TouchableOpacity } from "react-native";
 import { Auth } from "src/components/Auth";
+import {
+    CodeField,
+    Cursor,
+    useBlurOnFulfill,
+    useClearByFocusCell,
+} from 'react-native-confirmation-code-field';
 import * as S from "./styled"
+import { checkNumber } from "src/utils";
+
+const CELL_COUNT = 6;
 
 export const VerifyCodeScreen: React.FC = () => {
+    const [value, setValue] = useState('');
+    const codeFieldRef = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
     const [isDisabled, setIsDisabled] = useState<boolean>(true);
+    const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+        value,
+        setValue,
+    });
+
+    const onChangeText = (text: string) => {
+        const newText = checkNumber(text);
+        const codeValidationRegex = /^\d{6}$/;
+        codeValidationRegex.test(newText) ? setIsDisabled(false) : setIsDisabled(true);
+        setValue(newText);
+    };
+
     return (
         <Auth
             headerText={`인증 번호를 보냈어요!\n` + `받은 인증 번호를 입력해 주세요`}
             subHeaderText={
-                <S.VerifyCodeTextContainer>
+                <S.VerifyCodeScreenTextContainer>
                     <Text size="16">문자가 안 오나요?</Text>
                     <TouchableOpacity activeOpacity={0.2}>
                         <Text size="16" color={colors.primary}> 재전송 하기</Text>
                     </TouchableOpacity>
-                </S.VerifyCodeTextContainer>
+                </S.VerifyCodeScreenTextContainer>
             }
             bottomText="인증하기"
             isDisabled={isDisabled}
             onPress={() => console.log('인증하기')}
         >
-            <Text size="18">VerifyCode Screen</Text>
+            <CodeField
+                ref={codeFieldRef}
+                {...props}
+                value={value}
+                onChangeText={onChangeText}
+                cellCount={CELL_COUNT}
+                caretHidden={true}
+                keyboardType="number-pad"
+                textContentType="oneTimeCode"
+                rootStyle={{
+                    width: '100%',
+                }}
+                renderCell={({ index, symbol, isFocused }) => (
+                    <S.VerifyCodeScreenInput key={index} onLayout={getCellOnLayoutHandler(index)}>
+                        <Text size="20" fontFamily="medium">
+                            {symbol || (isFocused ? <Cursor /> : null)}
+                        </Text>
+                    </S.VerifyCodeScreenInput>
+                )}
+            />
         </Auth>
     )
 }
--- apps/auth/src/screens/verifyCode/styled.ts
+++ apps/auth/src/screens/verifyCode/styled.ts
@@ -1,7 +1,17 @@
 import styled from "@emotion/native";
+import { TextInput } from "react-native-gesture-handler";
 
-export const VerifyCodeTextContainer = styled.View` 
+export const VerifyCodeScreenTextContainer = styled.View` 
     flex-direction: row;
     align-items: center;
-    
+`;
+
+export const VerifyCodeScreenInput = styled(TextInput)`
+  width: 47px;
+  height: 67px;
+  padding: 0px 16px;
+  border-radius: 12px;
+  font-size: 20px;
+  background-color: #f4f4f5;
+  margin-top: 20px;
 `;
