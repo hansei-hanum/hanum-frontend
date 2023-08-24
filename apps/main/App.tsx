b02100cfe620e6b361d274924ff8d8a055e6095a
최근원 <kidjustinchoi@gmail.com>
2023-08-24 15:11:07 +0900

    feat(main): add devices in expo folder

--- apps/main/App.tsx
+++ apps/main/App.tsx
@@ -1,19 +1,18 @@
+import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { LogBox } from 'react-native';
 
-import { RecoilRoot } from 'recoil';
 import { Router } from 'src/Router';
 
 const client = new QueryClient();
 
 LogBox.ignoreAllLogs(true);
 
+
 export default function App() {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={client}>
-        <Router />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={client}>
+      <Router />
+    </QueryClientProvider>
   );
 }
