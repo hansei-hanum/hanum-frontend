5c3c26f811d29e517759dd6bd998f10062a1f61b
최근원 <kidjustinchoi@gmail.com>
2023-08-27 13:40:26 +0900

    feat: add lunch box component

--- src/screens/lunchTable/index.tsx
+++ src/screens/lunchTable/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import { Text } from 'src/components';
+import { Text, LunchBox } from 'src/components';
 
 import * as S from './styled';
 
 export const LunchTableScreen: React.FC = () => {
   return (
     <S.LunchTableScreenContainer>
-      <Text size="24" fontFamily="bold">
-        Cafeteria Screen
-      </Text>
+      <LunchBox
+        isPrimary={true}
+        date="5/27(수)"
+        lunch="현미밥, 불고기, 앙기모링, 스슈, 맛있당, 오믈라이스"
+      />
+      <LunchBox
+        isPrimary={false}
+        date="5/27(수)"
+        lunch="현미밥, 불고기, 앙기모링, 스슈, 맛있당, 오믈라이스"
+      />
+      <LunchBox
+        isPrimary={false}
+        date="5/27(수)"
+        lunch="현미밥, 불고기, 앙기모링, 스슈, 맛있당, 오믈라이스"
+      />
+      <LunchBox
+        isPrimary={false}
+        date="5/27(수)"
+        lunch="현미밥, 불고기, 앙기모링, 스슈, 맛있당, 오믈라이스"
+      />
     </S.LunchTableScreenContainer>
   );
 };
--- src/screens/lunchTable/styled.ts
+++ src/screens/lunchTable/styled.ts
@@ -2,6 +2,5 @@ import styled from '@emotion/native';
 
 export const LunchTableScreenContainer = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  padding: 70px 20px 20px 20px;
 `;
