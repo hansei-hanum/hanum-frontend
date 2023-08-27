8de2420acbc9dc8b4d29a58297061590b74027d8
최근원 <kidjustinchoi@gmail.com>
2023-08-28 08:57:32 +0900

    chore: del react-native-reanimated

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'],
   };
 };
