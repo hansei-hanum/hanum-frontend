51171611c52bba881ce1d1c327846e937c69c097
최근원 <kidjustinchoi@gmail.com>
2023-09-02 18:35:34 +0900

    fix(style): show more screen style issue fix

--- src/components/common/Icon/styled.ts
+++ src/components/common/Icon/styled.ts
@@ -1,9 +1,22 @@
+import { Platform } from 'react-native';
+
 import styled from '@emotion/native';
 
-import { colors } from 'src/styles';
+import { colors, fonts } from 'src/styles';
 
 export const IconWrapper = styled.View`
   padding: 4px;
   border-radius: 10px;
   background-color: ${colors.lightGray};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Icon = styled.Text`
+  font-size: 30px;
+  font-family: ${fonts.tossIcon};
+  color: ${colors.black};
+  text-align: center;
+  position: ${Platform.OS === 'ios' ? 'auto' : 'relative'};
+  top: ${Platform.OS === 'ios' ? '0px' : '5px'};
 `;
