0dfd0e51096923935148fd62066fede7a0392688
최근원 <kidjustinchoi@gmail.com>
2023-09-04 22:32:10 +0900

    feat: add web view

--- src/screens/home/styled.ts
+++ src/screens/home/styled.ts
@@ -12,3 +12,10 @@ export const HomeScreenContainer = styled.ScrollView`
   flex: 1;
   flex-direction: column;
 `;
+
+export const HomeScreenHeaderIconContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  column-gap: 20px;
+`;
---
+++ src/screens/webView/styled.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/native';
+
+export const WebViewSafeContainer = styled.SafeAreaView`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const HeaderContainer = styled.View`
+  padding: 0px 20px 0px 20px;
+  padding-left: 0;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const HeaderContentWrapper = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+export const LoadingWrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
