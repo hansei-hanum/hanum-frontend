f87406f5ca2cc5a122216496021cec3ab880db97
최근원 <kidjustinchoi@gmail.com>
2023-09-08 10:19:46 +0900

    del formatted department utils file

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,7 +1,6 @@
 export * from './auth';
 export * from './home';
 export * from './showMore';
-export * from './lunchTable';
 export * from './hanumPay';
 export * from './schedule';
 export * from './main';
