6c574e67043ff512d80f2c7d3135fe493ec76d9f
plkmnb100 <pcy06@kakao.com>
2023-09-13 10:42:30 +0900

    fix: FUCKING CAMERA PERMISSION BUG

--- ios/Hanum/AppDelegate.mm
+++ ios/Hanum/AppDelegate.mm
@@ -12,7 +12,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
