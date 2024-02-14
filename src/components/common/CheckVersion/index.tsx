2973d70921570576e04900bee5eace273f03064c
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-02-14 23:15:08 +0900

    fix(style): check version component style fix

--- src/components/common/CheckVersion/index.tsx
+++ src/components/common/CheckVersion/index.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import VersionCheck from 'react-native-version-check';
 import { Linking } from 'react-native';
 
-import { isAndroid, isIos } from 'src/utils';
+import { isAndroid } from 'src/utils';
 
 import { Modal } from '../Modal';
 import { Button } from '../Button';
+import { Text } from '../Text';
 
 const ANDROID_STORE_URL = 'https://play.google.com/store/apps/details?id=com.hanowl.hanum';
 const APP_STORE_URL = 'https://apps.apple.com/kr/app/%ED%95%9C%EC%9B%80/id6463776026?l';
@@ -20,15 +21,14 @@ export const CheckVersion: React.FC = () => {
     const CurrentVersion = VersionCheck.getCurrentVersion();
     //앱의 최신버전
     const LatestVersion = await VersionCheck.getLatestVersion();
-    console.log('LatestVersion', LatestVersion, 'CurrentVersion', CurrentVersion, isIos);
 
     //기기에 설치되있는 버전과 앱에 올려져있는 최신버전을 비교
     VersionCheck.needUpdate({
       currentVersion: CurrentVersion,
       latestVersion: LatestVersion,
-    }).then(({ isNeeded }) => {
+    }).then((res) => {
       setIsLoading(false);
-      setIsVersionCorrect(!isNeeded);
+      setIsVersionCorrect(!res.isNeeded);
     });
   };
 
@@ -43,8 +43,13 @@ export const CheckVersion: React.FC = () => {
   if (!isLoading && !isVersionCorrect) {
     return (
       <Modal
-        title="업데이트 확인"
-        text={'새로운 버전이 출시되었으니, 업데이트를 진행해주세요'}
+        title="업데이트 안내"
+        linkText={
+          <Text size={16} isCenter>
+            {isAndroid ? 'Google Play' : 'App Store'}에 필수 업데이트가 있어요{'\n'}
+            앱을 사용하려면 업데이트를 진행해 주세요
+          </Text>
+        }
         modalVisible={true}
         button={<Button onPress={onButtonPress}>업데이트 하기</Button>}
       />
