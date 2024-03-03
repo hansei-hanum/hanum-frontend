29c84a78640286e9cdc1dea3ff7f1b58e05b9ec1
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-03-03 18:28:45 +0900

    feat: add reply likes

--- src/components/community/postDetail/CommentCard/styled.ts
+++ src/components/community/postDetail/CommentCard/styled.ts
@@ -63,3 +63,17 @@ export const PostCommentImageWrapper = styled(TouchableOpacity)`
   justify-content: center;
   align-items: center;
 `;
+
+export const PostDetailLayoutContainer = styled.View`
+  flex: 1;
+  row-gap: 24px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+export const PostDetailLayoutReplyContainer = styled.View`
+  flex-direction: row;
+  column-gap: 10px;
+  align-items: center;
+`;
