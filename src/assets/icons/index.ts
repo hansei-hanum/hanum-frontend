1d09f64781e4296bb696bd94821ea1b47acc6a1a
PARK CHAN YOUNG <pcy06@kakao.com>
2024-03-17 14:53:52 +0900

    chore: remove unused svg icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,8 +1,2 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-export const MealIcon = require('./mealIcon.svg');
-export const TimerIcon = require('./timerIcon.svg');
-export const ScheduleIcon = require('./schedule.svg');
-export const PayIcon = require('./pay.svg');
-export const EoullimIcon = require('./party.svg');
-export const CalendarIcon = require('./calendar.svg');
-export const VerifyCheckIcon = require('./verifyCheckIcon.svg');
+export const VerifyCheckIcon = require('./verifyCheckIcon.png');
